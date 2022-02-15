57d8111584773cc6398ed86de18e2eb52a797dbc
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-02-15 13:41:01 -0500

    commit tree e3cd8dda67d249e6eafcf483742711ea3922b0e9

---
+++ index.js
@@ -0,0 +1,95 @@
+
+    
+
+ 
+function divideBy3() {
+return (number /= 3);
+ }
+
+divideBy3(); //=> 3.3333333333333335
+3.3333333333333335
+
+add5(); //=> 8.333333333333334
+8.333333333333334
+
+// reset number
+
+number = 10;
+10
+
+add5(); //=> 15
+15
+
+divideBy3(); //=> 5
+5
+let number = 5;
+
+ 
+number++; //=> 5... hmmmm
+5
+
+number; //=> 6 -- the number was incremented after it was evaluated
+6
+
+number--; //=> 6
+6
+number; //=> 5
+5
+--number; //=> 4
+4
+
+number; //=> 4
+4
+
+++number; //=> 5
+5
+
+number; //=> 5
+5
+number += 3; //=> 8
+8
+number -= 2; //=> 6
+6
+number *= 10; //=> 60
+60
+number /= 5; //=> 12
+12
+number = 10;
+10
+
+function add5() {
+ return (number += 5);
+ }
+
+
+function divideBy3() {
+ return (number /= 3);
+ }
+
+
+ divideBy3(); //=> 3.3333333333333335
+3.3333333333333335
+ 
+ add5(); //=> 8.333333333333334
+8.333333333333334
+
+ // reset number
+
+number = 10;
+10
+ 
+add5(); //=> 15
+15
+
+divideBy3(); //=> 5
+5
+parseInt("2", 10); //=> 2
+2
+parseInt("2.2222", 10);
+2
+parseInt("nonsense!", 10);
+NaN
+parseFloat("80.123999"); // 80.123999
+80.123999
+
+
