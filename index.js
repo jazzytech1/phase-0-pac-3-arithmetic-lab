8a338c20ab029d98e05bf469fc5aebfaf19cb69c
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-02-15 19:35:23 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a,b) {
+    return a+b;
+}
+//add(1,80);
+
+function subtract(a,b) {
+    return a-b;
+}
+//subtract(60,40);
+
+function multiply(a,b) {
+    return a*b;    
+}
+//multiply (2*3.4);
+
+function divide(a,b) {
+    return a/b;
+}
+//divide (5.0/2.5);
+
+function increment(n){
+    return n+1;
+}
+//increment(5);
+
+function decrement(n){
+    return n-1;
+}
+//decrement(5);
+
+function makeInt (n){
+   const parsed= parseInt(n,10);
+    return parsed
+}
+
+function makeInt (n){
+    const parsed= parseInt(n,10);
+     return parsed
+ }
+function preserveDecimal (n) {
+    const parsed= parseFloat(2.222);
+    return parsed
+} 
+function preserveDecimal (n) {
+    const parsed= parseFloat(n,2.222);
+    return parsed
+} 
